9d5467c6cbd418043b117f96412c6ed14ee753e9
IfanantenanaJeannot <127655708+IfanantenanaJeannot@users.noreply.github.com>
2023-10-19 13:25:35 +0300

    Delete animationSouris.js

--- animationSouris.js
+++
@@ -1,6 +0,0 @@
-const mousemove = document.getElementById('mousemove');
-
-window.addEventListener("mousemove", (e) => {
-    mousemove.style.left = (e.pageX + 1)+ "px";
-    mousemove.style.top = e.pageY + "px";
-});
